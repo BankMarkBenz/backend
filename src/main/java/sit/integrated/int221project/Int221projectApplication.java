a94923427627b35dff66bcd7332f8349bdd664d4
bankhubjar <jakkrapop.sit@mail.kmutt.ac.th>
2021-04-21 17:16:48 +0700

    init commit

---
+++ src/main/java/sit/integrated/int221project/Int221projectApplication.java
@@ -0,0 +1,19 @@
+package sit.integrated.int221project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Int221projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Int221projectApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/sit/integrated/int221project/Int221projectApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.integrated.int221project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Int221projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
