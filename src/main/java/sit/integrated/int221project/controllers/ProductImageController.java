025ce99ca44d2dc8621b8b5c079f3eabb54b3f4d
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-05-05 15:36:02 +0700

    [UPDATE] Exception and Improve image Controller

--- src/main/java/sit/integrated/int221project/controllers/ProductImageController.java
+++ src/main/java/sit/integrated/int221project/controllers/ProductImageController.java
@@ -6,6 +6,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import sit.integrated.int221project.Exception.RequestException;
+import sit.integrated.int221project.Exception.ResponseException;
 import sit.integrated.int221project.models.Products;
 import sit.integrated.int221project.repositories.ProductsRepository;
 
@@ -24,18 +26,24 @@ public class ProductImageController{
     @Autowired
     private ProductsRepository productRepository;
 
-    @GetMapping("/get/{id:.+}")
-    public ResponseEntity<byte[]> getImage(@PathVariable("id")String id) throws IOException {
-        FileInputStream fi = new FileInputStream("picture"+id);
-        byte[] image = fi.readAllBytes();
-        fi.close();
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+    @GetMapping("/get/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id")String id){
+        try {
+            FileInputStream fi = new FileInputStream("picture" + id);
+            byte[] image = fi.readAllBytes();
+            fi.close();
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+        }catch(Exception e){
+           throw new RequestException("Image not Found");
+        }
     }
 
     @PostMapping ("/add/{id}")
     public ResponseEntity<Object> fileUpload(@RequestParam("File") MultipartFile file, @PathVariable("id")String id)throws IOException{
+        if(hasFoundId(parseInt(id))== false){
+            throw new RequestException("Image Id Not Found");
+        }
         File myFile = new File("picture" + id);
-        System.out.println("1111");
         if(myFile.createNewFile()) {
             FileOutputStream fos = new FileOutputStream(myFile);
             fos.write(file.getBytes());
@@ -44,15 +52,18 @@ public class ProductImageController{
         return  new ResponseEntity<>("The File Uploaded Successfully", HttpStatus.OK);
     }
 
-    @PutMapping("/edit/{id:.+}")
+    @PutMapping("/edit/{id}")
     public ResponseEntity<Object> changeImage(@RequestParam("File")MultipartFile file,@PathVariable("id")String id)throws IOException {
+        if(hasFoundId(parseInt(id))== false){
+            throw new RequestException("Image Id Not Found");
+        }
         FileOutputStream fos = new FileOutputStream("picture"+id);
         fos.write(file.getBytes());
         fos.close();
         return  new ResponseEntity<>("The File Change Successfully", HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id:.+}")
+    @DeleteMapping("/delete/{id}")
     public void deleteImage(@PathVariable("id")String id){
         String IdString[] = id.split(":");
         int hasId = parseInt(IdString[1]);
@@ -71,5 +82,11 @@ public class ProductImageController{
         }
         return false;
     }
+
+    @RequestMapping("/")
+    public ResponseEntity<ResponseException> handleItemNotFoundException(String exception){
+        throw new RequestException(exception);
+    }
+
 
 }
