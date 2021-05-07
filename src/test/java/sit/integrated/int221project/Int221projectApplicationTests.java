c2d8491557622917521d7f443faf07b2f1baf3d3
bankhubjar <bankneedone@gmail.com>
2021-05-07 10:33:47 +0700

    add test

--- src/test/java/sit/integrated/int221project/Int221projectApplicationTests.java
+++ src/test/java/sit/integrated/int221project/Int221projectApplicationTests.java
@@ -2,12 +2,20 @@ package sit.integrated.int221project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import sit.integrated.int221project.repositories.ColorsRepository;
+import sit.integrated.int221project.repositories.ProductsRepository;
 
 @SpringBootTest
 class Int221projectApplicationTests {
 
+    private ProductsRepository productsRepository;
+    private ColorsRepository colorsRepository;
+
     @Test
     void contextLoads() {
+        Assert.notEmpty(productsRepository.findAll());
+        Assert.notEmpty(colorsRepository.findAll());
     }
 
 }
